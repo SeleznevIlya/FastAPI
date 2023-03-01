75a0ac6f090c8b76fd906bddfc0b1cba764cdbe7
Илья Селезнев <ilya_seleznev@MacBook-Air-Ila.local>
2023-03-01 22:17:54 +0300

    introduction to the redis

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from auth.router import router as auth_router
 from posts.router import router as post_router
+from redis import asyncio as aioredis
+
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+
 
 app = FastAPI(
     title='My FastAPI App'
@@ -8,3 +14,9 @@ app = FastAPI(
 
 app.include_router(auth_router)
 app.include_router(post_router)
+
+
+@app.on_event("startup")
+async def startup_event():
+    redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
